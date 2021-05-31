261f2ab3acb925147dffbdde525b1dae5ce7e7a3
markoknezovic <58472152+markoknezovic@users.noreply.github.com>
2021-05-31 09:21:34 +0200

    Add files via upload

---
+++ zavrsnoponavljanje/zavrsnoponavljanje/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavrsnoponavljanje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutes, seconds, unos;
+            Console.WriteLine("upisi sekunde:");
+            
+            unos = Convert.ToInt32(Console.ReadLine());
+            minutes = unos / 60;
+            Console.WriteLine(minutes);
+            seconds = unos - (minutes*60);
+            Console.WriteLine(seconds);
+            Console.ReadKey();
+        }
+    }
+}
